dab93ebdf86aa44124752bbb0a4a6539a2abcd09
silenthill99 <florian.graziani@sfr.fr>
2025-09-28 23:00:35 +0200

    Tentative d'installation de Flowbite React, sans succès.

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
+    plugins: [laravel({
+        input: ['resources/css/app.css', 'resources/js/app.tsx'],
+        ssr: 'resources/js/ssr.tsx',
+        refresh: true,
+    }), react(), tailwindcss(), flowbiteReact()],
     esbuild: {
         jsx: 'automatic',
     },
