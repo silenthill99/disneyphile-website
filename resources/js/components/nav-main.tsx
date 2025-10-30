8ee69e9d981934b1e283d48f3272db5ec4096f5e
silenthill99 <florian.graziani@sfr.fr>
2025-10-30 13:11:24 +0100

    Modification de la sidebar

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,4 +1,4 @@
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem, SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 
@@ -6,32 +6,42 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
     const {auth} = usePage<SharedData>().props
     return (
-        <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Platform</SidebarGroupLabel>
-            <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton
-                            asChild isActive={item.href === page.url}
-                            tooltip={{ children: item.title }}
-                        >
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-                {auth.user && (
-                    <SidebarMenuItem>
-                        <SidebarMenuButton asChild isActive={"/tags" === page.url} tooltip={{ children: "Essai"}}>
-                            <Link href={"/tags"} prefetch>
-                                <span>Tags</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                )}
-            </SidebarMenu>
-        </SidebarGroup>
+        <>
+            <SidebarGroup className="px-2 py-0">
+                <SidebarGroupLabel>Platform</SidebarGroupLabel>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                asChild isActive={item.href === page.url}
+                                tooltip={{ children: item.title }}
+                            >
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+
+                </SidebarMenu>
+            </SidebarGroup>
+            <SidebarGroup>
+                <SidebarGroupLabel>Partie administration</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        {auth.user && (
+                            <SidebarMenuItem>
+                                <SidebarMenuButton asChild isActive={"/tags" === page.url} tooltip={{ children: "Essai"}}>
+                                    <Link href={"/tags"} prefetch>
+                                        <span>Tags</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        )}
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </>
     );
 }
